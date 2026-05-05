c11fad59ac51c52bde83e9ae1029d0f572199a1e
yogisha <yogisha9g@gmail.com>
2026-05-05 12:26:37 +0530

    empty

--- app/core/firebase.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, auth
-
-cred = credentials.Certificate("C:\Users\yogis\Desktop\Learn\serviceAccountKey.json")
-
-firebase_admin.initialize_app(cred)
--- app/schemas/auth_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class RequestOTPRequest(BaseModel):
-    phone: str
-
-class FirebaseLoginRequest(BaseModel):
-    firebase_token: str
